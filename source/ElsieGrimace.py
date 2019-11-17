ee4373fd4be26a3443409d60a4a0eb0ae2dc045a
Dan Lou <loudan1980@gmail.com>
2019-11-17 10:25:06 -0800

    update applications

--- source/ElsieGrimace.py
+++ source/ElsieGrimace.py
@@ -1,4 +1,4 @@
-import requests
+import requests, time
 
 ip = '192.168.1.213'
 
@@ -6,4 +6,7 @@ sound_play = 'http://'+ip+'/api/audio/play'
 requests.post(sound_play,json={"AssetId": 's_Ecstacy.wav', 'Volume': 100})
 
 image_display = 'http://'+ip+'/api/images/display'
-requests.post(image_display,  json={'FileName': ‘e_JoyGoofy3.jpg', 'TimeOutSeconds': 5})
+requests.post(image_display,  json={'FileName': 'e_JoyGoofy3.jpg'})
+
+time.sleep(5)
+requests.post(image_display,  json={'FileName': 'e_DefaultContent.jpg'})
